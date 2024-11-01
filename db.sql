fc82d271bc12d9c8ff788e111f54aa6a85e0fd52
Ismail Sherif <ismailsherifwork@gmail.com>
2024-11-02 01:34:39 +0200

    Make user_gender field optional in users table

--- db.sql
+++ db.sql
@@ -6,8 +6,8 @@ USE ml_db;
 CREATE TABLE users (
   user_id INT PRIMARY KEY AUTO_INCREMENT,
   user_name VARCHAR(255) NOT NULL,
-  user_gender VARCHAR(255) NOT NULL,
   user_code VARCHAR(255) NOT NULL,
+  user_gender VARCHAR(255),
   user_username VARCHAR(255) NOT NULL,
   user_password VARCHAR(255) NOT NULL,
   user_additional_info TEXT,
