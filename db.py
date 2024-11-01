d48075471e87ede745a8758d1fa59f5170efc5ab
Ismail Sherif <ismailsherifwork@gmail.com>
2024-11-02 01:41:57 +0200

    Update database connection to use DB_PASS instead of DB_PASSWORD

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ load_dotenv()
 db = mysql.connector.connect(
     host = os.getenv("DB_HOST"),
     user = os.getenv("DB_USER"),
-    password = os.getenv("DB_PASSWORD"),
+    password = os.getenv("DB_PASS"),
     port = os.getenv("DB_PORT"),
     database = os.getenv("DB_NAME"),
 )
