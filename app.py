4ff1c8b73483884ea9bcd0a0ede72555a8861462
Ismail Sherif <ismailsherifwork@gmail.com>
2024-11-02 02:13:23 +0200

    Implement login functionality and update database connection handling

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 import flask
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
-from db import db
+import os
+from dotenv import load_dotenv
+import db
 
+load_dotenv()
 app = flask.Flask(__name__)
 api = Api(app)
 CORS(app)
 
 class Login(Resource):
     def post(self):
-        print(flask.request.json)
+        data = flask.request.json
+        print("Request Data Recieved:", data)
+        cursor = db.cursor()
+        cursor.execute(f"""
+            SELECT * FROM users
+            WHERE user_username = "{data["username"]}" AND user_password = "{data["password"]}"
+        """)
+        result = cursor.fetchall()
+        print("Result:", result)
+        if len(result) == 0:
+            return {"data": "Invalid credentials"}
+        else:
+            return {"data": "Login successful"}
+        # db.commit()
+
         
-        return {"data": "Registration data"}
+
+        # return {"data": "Registration data"}
 
 api.add_resource(Login, "/api/login")
 
--- db.py
+++ db.py
@@ -1,34 +1,22 @@
 import os
 import mysql.connector
-from dotenv import load_dotenv
 
-load_dotenv()
+def create_conn():
+    db = mysql.connector.connect(
+        host = os.getenv("DB_HOST"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASS"),
+        port = os.getenv("DB_PORT"),
+        database = os.getenv("DB_NAME"),
+    )
+    return db
 
-db = mysql.connector.connect(
-    host = os.getenv("DB_HOST"),
-    user = os.getenv("DB_USER"),
-    password = os.getenv("DB_PASS"),
-    port = os.getenv("DB_PORT"),
-    database = os.getenv("DB_NAME"),
-)
+# cursor.execute(f"""
+# """)
+# db.commit()
 
-user_name = "Ismail Sherif"
-user_code = "IS-0001"
-user_gender = "Male"
-user_username = "ismail"
-user_password = "helloworld"
-user_additional_info = "He is a senior cs student."
+# result = cursor.fetchall()
+# for row in result:
+#     print(row)
 
-cursor = db.cursor()
-cursor.execute(f"""
-    INSERT INTO users (user_name, user_code, user_gender, user_username, user_password, user_additional_info)
-    VALUES ("{user_name}", "{user_code}", "{user_gender}", "{user_username}", "{user_password}", "{user_additional_info}")
-""")
-db.commit()
-
-cursor.execute("SELECT * FROM users")
-result = cursor.fetchall()
-for row in result:
-    print(row)
-
-db.close()
+# db.close()
